4b312584176db0758b94382ffb83e1d08876b490
salaboy <Salaboy@gmail.com>
2020-06-01 15:02:04 +0100

    Update DemoApplication.java

--- src/main/java/com/salaboy/conferences/agenda/DemoApplication.java
+++ src/main/java/com/salaboy/conferences/agenda/DemoApplication.java
@@ -46,7 +46,7 @@ public class DemoApplication {
 
     @GetMapping("/info")
     public String getInfo() {
-        return "{ \"name\" : \"Agenda Service\", \"version\" : \"" + version + "++\", \"source\": \"https://github.com/salaboy/fmtok8s-agenda/releases/tag/v" + version + "\" }";
+        return "{ \"name\" : \"Agenda Service\", \"version\" : \"" + version + "\", \"source\": \"https://github.com/salaboy/fmtok8s-agenda/releases/tag/v" + version + "\" }";
     }
 
     @PostMapping()
