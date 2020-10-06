f50a261f3482415521ee51da925986f2723fa3b2
Matheus Henrique de Sousa Cruz <matheus.cruz@radixeng.com.br>
2020-10-06 18:43:52 -0300

    Implementing tests

---
+++ src/test/java/com/salaboy/conferences/agenda/AgendaItemCreator.java
@@ -0,0 +1,21 @@
+package com.salaboy.conferences.agenda;
+
+import com.salaboy.conferences.agenda.model.AgendaItem;
+
+public class AgendaItemCreator {
+
+    public static final String DAY = "2020-10-05";
+    public static final String OTHER_DAY = "2020-10-02";
+
+    public static AgendaItem validWithDefaultDay() {
+        return new AgendaItem("Title", "Author", DAY, "13:00");
+    }
+
+    public static AgendaItem otherValidWithDefaultDay() {
+        return new AgendaItem("Other title", "Other Author", DAY, "13:00");
+    }
+
+    public static AgendaItem withFail() {
+        return new AgendaItem("Title fail", "Author fail",  OTHER_DAY, "12:30");
+    }
+}
